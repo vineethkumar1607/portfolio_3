699c3a5dabb000dba08086e3468fb198f820c895
Vineeth <vineethkumar1607@gmail.com>
2025-11-11 20:01:14 +0530

    Add GitHub Actions auto-deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,10 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
+      // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  
+  base: "/portfolio_3/",
 });
